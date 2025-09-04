2d5861d734bd70c3946b70401fdb75719b766790
gathering0715 <gathering0715@naver.com>
2025-09-04 17:06:34 +0900

    update

--- front-server/vite.config.js
+++ front-server/vite.config.js
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,     // 0.0.0.0 shortcut
+    host: true, // 0.0.0.0 과 동일
     port: 5173,
     strictPort: true,
-    allowedHosts: ['*'],
-    origin: '*',
+    cors: true,
+    allowedHosts: [
+      'localhost',
+      '.elasticbeanstalk.com', // 모든 EB 도메인 허용
+      'hash-proj-v2.eba-ijnehrgn.ap-northeast-2.elasticbeanstalk.com'
+    ],
   }
 })
